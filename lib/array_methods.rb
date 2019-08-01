74bcb2caffd73655e98f53665ce83028f73d51db
James Nash <nash.jamesm@gmail.com>
2019-08-01 20:40:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,8 @@
 def find_element_index(array, value_to_find)
-  
+  array.length.times do |count|
+    if array[count] == value_to_find 
+      return count
+    end
 end
 
 def find_max_value(array)
